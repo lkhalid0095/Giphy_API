290454d39da2a8ffc66a598e19cc6cb12000cebe
Farai Major <faraimutukumira@gmail.com>
2022-01-14 18:05:13 -0500

    added nav bar and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Giphy from "./components/Giphy";
+import Navbar from "./components/NavBar";
 
 import "./App.css";
 
 const App = () => {
-    return <div>     
-            <Giphy />
-            </div>
+    return <div>
+        <Navbar />
+        <Giphy />
+    </div>
 };
 
 export default App
